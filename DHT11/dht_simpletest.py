f6c83085192c2f8c390046f5a1a14a7c5f373f8d
Florentin Schäfer <florentin.schaefer@gmail.com>
2020-12-15 10:47:26 +0100

    added main to dht_simpletest.py

--- DHT11/dht_simpletest.py
+++ DHT11/dht_simpletest.py
@@ -2,16 +2,17 @@ import time
 import board
 import adafruit_dht
 
-dhtDevice = adafruit_dht.DHT11(board.D26, use_pulseio=False)
+if __name__ == "__main__":
+    dhtDevice = adafruit_dht.DHT11(board.D26, use_pulseio=False)
 
-while True:
-    try:
-        temperature = dhtDevice.temperature
-        humidity = dhtDevice.humidity
-        print("{}: Temp: {:.1f} C    Humidity: {}% ".format(time.asctime(time.localtime(time.time())), temperature,
-                                                            humidity))
+    while True:
+        try:
+            temperature = dhtDevice.temperature
+            humidity = dhtDevice.humidity
+            print("{}: Temp: {:.1f} C    Humidity: {}% ".format(time.asctime(time.localtime(time.time())), temperature,
+                                                                humidity))
 
-    except Exception as error:
-        continue
+        except Exception as error:
+            continue
 
-    time.sleep(10)
+        time.sleep(10)
