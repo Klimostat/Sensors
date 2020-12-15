4c2c4d855ee2ad4cdd65e88472b62f2026631e30
Florentin Schäfer <florentin.schaefer@gmail.com>
2020-12-15 08:43:12 +0100

    Simple script to measure temperature and humidity

---
+++ DHT11/dht_simpletest.py
@@ -0,0 +1,16 @@
+import time
+import board
+import adafruit_dht
+ 
+dhtDevice = adafruit_dht.DHT11(board.D26, use_pulseio=False)
+ 
+while True:
+	try:
+		temperature = dhtDevice.temperature
+		humidity = dhtDevice.humidity
+		print("{}: Temp: {:.1f} C    Humidity: {}% ".format(time.asctime(time.localtime(time.time())), temperature, humidity))
+ 
+	except Exception as error:
+		continue
+ 
+	time.sleep(10)
