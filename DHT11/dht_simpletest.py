b5851bb913bc7758fd63058afa1209fe69457859
Florentin Schäfer <florentin.schaefer@gmail.com>
2020-12-16 09:12:55 +0100

    made dht11-script object oriented for later use

--- DHT11/dht_simpletest.py
+++ DHT11/dht_simpletest.py
@@ -2,17 +2,34 @@ import time
 import board
 import adafruit_dht
 
+
+class DHT11:
+    def __init__(self, port=board.D26):
+        self.dhtDevice = adafruit_dht.DHT11(port, use_pulseio=False)
+
+    def get_temperature(self):
+        while True:
+            try:
+                temperature = self.dhtDevice.temperature
+                return temperature
+            except Exception:
+                continue
+
+    def get_humidity(self):
+        while True:
+            try:
+                humidity = self.dhtDevice.humidity
+                return humidity
+            except Exception:
+                continue
+
+
 if __name__ == "__main__":
-    dhtDevice = adafruit_dht.DHT11(board.D26, use_pulseio=False)
+    dht11 = DHT11()
 
     while True:
-        try:
-            temperature = dhtDevice.temperature
-            humidity = dhtDevice.humidity
-            print("{}: Temp: {:.1f} C    Humidity: {}% ".format(time.asctime(time.localtime(time.time())), temperature,
-                                                                humidity))
-
-        except Exception as error:
-            continue
-
+        temperature = dht11.get_temperature()
+        humidity = dht11.get_humidity()
+        print("{}: Temp: {:.1f} C    Humidity: {}% ".format(time.asctime(time.localtime(time.time())), temperature,
+                                                            humidity))
         time.sleep(10)
