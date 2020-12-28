65941e92333cc5110195ce66c794414ea3b51ddc
Florentin Schäfer <florentin.schaefer@gmail.com>
2020-12-28 15:33:51 +0100

    added WaterLevelSensor to get_sensor_data.py

--- get_sensor_data.py
+++ get_sensor_data.py
@@ -1,5 +1,6 @@
 from DHT11.dht11 import DHT11
 from MHZ14A.mhz14a import MHZ14A
+from Wassersensor.waterlevelsensor import WaterLevelSensor
 import time
 import mariadb
 import sys
@@ -14,10 +15,12 @@ def insert_data(cursor, value, sensor, timestamp):
 def get_sensor_data():
     mhz14a = MHZ14A()
     dht11 = DHT11()
+    waterlevelsensor = WaterLevelSensor()
 
     temperature = dht11.get_temperature()
     humidity = dht11.get_humidity()
     co2level = mhz14a.get_co2level()
+    water_ingress = waterlevelsensor.detect_water_ingress()
 
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -38,11 +41,13 @@ def get_sensor_data():
     insert_data(cur, str(temperature), "1", timestamp)
     insert_data(cur, str(humidity), "2", timestamp)
     insert_data(cur, str(co2level), "3", timestamp)
+    insert_data(cur, str(water_ingress), "4", timestamp)
     conn.commit()
     conn.close()
 
-    return "{}: Temp: {:.1f} C    Humidity: {}%    CO2: {} ppm".format(time.asctime(time.localtime(time.time())),
-                                                                       temperature, humidity, co2level)
+    return "{}: Temp: {:.1f} C    Humidity: {}%    CO2: {} ppm    Waterimgress: {}".format(
+        time.asctime(time.localtime(time.time())),
+        temperature, humidity, co2level, water_ingress)
 
 
 if __name__ == "__main__":
