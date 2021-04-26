a88e47c1aa74934c2cbe27a1ee24811f3fee87bd
Florentin Schäfer <7054@htl.rennweg.at>
2021-04-26 17:39:15 +0200

    def für thresholds in configurations.py hinzugefügt

--- configurations.py
+++ configurations.py
@@ -1,6 +1,15 @@
+import uio
+import ujson
+
 STATION_ID = 1
 TOKEN = "asdf"
 INTERVAL = 10
 
 WIFI_SSID = ""
 WIFI_PASSWD = ""
+
+def get_thresholds():
+    thresholds = uio.open("thresholds.json", "w")
+    json_data = ujson.load(thresholds)
+    thresholds.close()
+    return json_data
