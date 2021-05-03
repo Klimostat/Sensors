e9667f94acbc3645e683d4802c2c08cb3c5b2f49
Florentin <florentin.schaefer@gmail.com>
2021-05-03 16:46:01 +0200

    update config for HTLW3R_WLAN

--- configurations.py
+++ configurations.py
@@ -5,8 +5,8 @@ STATION_ID = 1
 TOKEN = "asdf"
 INTERVAL = 10
 
-WIFI_SSID = ""
-WIFI_PASSWD = ""
+WIFI_SSID = "HTLW3R_WLAN"
+WIFI_PASSWD = "HTLW3R_WLAN"
 
 def get_thresholds():
     thresholds = uio.open("thresholds.json", "w")
