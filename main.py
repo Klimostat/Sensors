200d75f04d848917c079d68484204e145175ceb2
Florentin Schäfer <7054@htl.rennweg.at>
2021-05-10 17:01:13 +0200

    def main() hinzugefügt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import wifi_connect
 import thresholds
-
-wifi_connect.connect()
-thresholds.update_thresholds()
-
 import utime
 import configurations
 import ujson
@@ -12,33 +8,42 @@ import gc
 from machine import Pin, SoftI2C
 from scd30 import SCD30
 
-url = "{}receive.php".format(configurations.API_ENDPOINT)
-headers = {"content-type": "application/x-www-form-urlencoded"}
 
-i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
-scd30 = SCD30(i2c, addr=0x61)
+def main():
+    wifi_connect.connect()
+    thresholds.update_thresholds()
 
-while True:
-    last_time = utime.time()
+    url = "{}receive.php".format(configurations.API_ENDPOINT)
+    headers = {"content-type": "application/x-www-form-urlencoded"}
 
-    while scd30.get_status_ready() != 1:
-        utime.sleep_ms(200)
-    co2, temp, relh = scd30.read_measurement()
+    i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
+    scd30 = SCD30(i2c, addr=0x61)
 
-    data = "data=" + ujson.dumps({
-        "id": configurations.STATION_ID,
-        "token": configurations.TOKEN,
-        "co2": co2,
-        "temp": temp,
-        "relh": relh
-    })
-    print(data)
-    try:
-        thresholds_obj = ujson.loads(urequests.post(url, headers=headers, data=data).text)
-        thresholds.update_thresholds(thresholds_obj)
-    except ValueError:
-        # TODO: Serververbindung fehlgeschlagen
-        pass
+    while True:
+        last_time = utime.time()
 
-    gc.collect()
-    utime.sleep(last_time + configurations.INTERVAL - utime.time())
+        while scd30.get_status_ready() != 1:
+            utime.sleep_ms(200)
+        co2, temp, relh = scd30.read_measurement()
+
+        data = "data=" + ujson.dumps({
+            "id": configurations.STATION_ID,
+            "token": configurations.TOKEN,
+            "co2": co2,
+            "temp": temp,
+            "relh": relh
+        })
+        print(data)
+        try:
+            thresholds_obj = ujson.loads(urequests.post(url, headers=headers, data=data).text)
+            thresholds.update_thresholds(thresholds_obj)
+        except ValueError:
+            # TODO: Serververbindung fehlgeschlagen
+            pass
+
+        gc.collect()
+        utime.sleep(last_time + configurations.INTERVAL - utime.time())
+
+
+if __name__ == "__main__":
+    main()
