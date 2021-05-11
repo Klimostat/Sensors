7b95683f43decf3541ce2185d4b84277fbe6aeeb
Florentin <florentin.schaefer@gmail.com>
2021-05-11 13:37:25 +0200

    Fehlertoleranz hinzugefügt falls Sensor nicht korrekt antwortet

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import utime
 import configurations
 import ujson
 import urequests
+import uerrno
 import gc
 from machine import Pin, SoftI2C
 from scd30 import SCD30
@@ -22,27 +23,30 @@ def main():
     while True:
         last_time = utime.time()
 
-        while scd30.get_status_ready() != 1:
-            utime.sleep_ms(2000)
-        co2, temp, relh = scd30.read_measurement()
-
-        data = "data=" + ujson.dumps({
-            "id": configurations.STATION_ID,
-            "token": configurations.TOKEN,
-            "co2": co2,
-            "temp": temp,
-            "relh": relh
-        })
-        print(data)
         try:
-            thresholds_obj = ujson.loads(urequests.post(url, headers=headers, data=data).text)
-            thresholds.update_thresholds(thresholds_obj)
-        except ValueError:
-            # TODO: Serververbindung fehlgeschlagen
-            pass
+            while scd30.get_status_ready() != 1:
+                utime.sleep_ms(200)
+            co2, temp, relh = scd30.read_measurement()
 
-        gc.collect()
-        utime.sleep(last_time + configurations.INTERVAL - utime.time())
+            data = "data=" + ujson.dumps({
+                "id": configurations.STATION_ID,
+                "token": configurations.TOKEN,
+                "co2": co2,
+                "temp": temp,
+                "relh": relh
+            })
+            print(data)
+            try:
+                thresholds_obj = ujson.loads(urequests.post(url, headers=headers, data=data).text)
+                thresholds.update_thresholds(thresholds_obj)
+            except ValueError:
+                # TODO: Serververbindung fehlgeschlagen
+                pass
+
+            gc.collect()
+            utime.sleep(last_time + configurations.INTERVAL - utime.time())
+        except OSError as err:
+            print("An error occurred: {}".format(uerrno.errorcode[err.args[0]]))
 
 
 if __name__ == "__main__":
